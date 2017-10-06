0c6c119d7f27ec2122849820d87a9e2434320447
Rajesh K <rajeshtechgeek@gmail.com>
2017-10-06 19:29:19 +0530

    splash,login screens completed

--- app/src/main/java/com/numnu/android/MainActivity.java
+++ app/src/main/java/com/numnu/android/MainActivity.java
@@ -1,7 +1,16 @@
 package com.numnu.android;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.textView3);
+        textView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    public void createAccount(View view) {
+        Intent mainIntent = new Intent(MainActivity.this,SignupActivity.class);
+        MainActivity.this.startActivity(mainIntent);
+
+    }
+
+    public void facebookSignIn(View view) {
+    }
+
+    public void signIn(View view) {
+        Intent mainIntent = new Intent(this,LoginActivity.class);
+        this.startActivity(mainIntent);
     }
 }
+
+
+
+
+
