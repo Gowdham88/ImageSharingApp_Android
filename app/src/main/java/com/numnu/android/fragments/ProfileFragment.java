2d6b7d63f0a933e44af40adaa4988dbd3458a17c
Rajesh K <rajeshtechgeek@gmail.com>
2017-10-14 18:51:12 +0530

    profile,home page search design

--- app/src/main/java/com/numnu/android/fragments/ProfileFragment.java
+++ app/src/main/java/com/numnu/android/fragments/ProfileFragment.java
@@ -2,11 +2,10 @@ package com.numnu.android.fragments;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,63 +39,26 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_profile, container, false);
-         TextView shareApp = view.findViewById(R.id.text_share_app);
-        shareApp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                shareApp();
-            }
-        });
-
-        TextView rateApp = view.findViewById(R.id.text_rate_app);
-        rateApp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                rateApp();
-            }
-        });
-
-        TextView terms = view.findViewById(R.id.text_terms);
-        terms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTerms();
-            }
-        });
-
-        TextView privacy = view.findViewById(R.id.text_privacy);
-        privacy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showPrivacyPolicy();
-            }
-        });
 
         TextView toolbarTitle=view.findViewById(R.id.toolbar_title);
-        toolbarTitle.setText("Settings");
+        toolbarTitle.setText(R.string.edit_profile);
+
+        view.findViewById(R.id.imageView_profile_edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editProfile();
+            }
+        });
+
         return view;
     }
 
-    private void showPrivacyPolicy() {
+    private void editProfile() {
+
+
 
     }
 
-    private void showTerms() {
-
-    }
-
-    private void rateApp() {
-        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + context.getPackageName())));
-    }
-
-    private void shareApp() {
-
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT,"Find Delicious Food around you.Get Numnu App. Click to Download https://play.google.com/store/apps/details?id="+context.getPackageName());
-        sendIntent.setType("text/plain");
-        context.startActivity(Intent.createChooser(sendIntent, context.getResources().getText(R.string.share_using)));
-    }
 
 
 }
