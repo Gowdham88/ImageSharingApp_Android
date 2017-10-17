3176a93c82b2cb785707575d2c1050dbfd64cd21
Rajesh K <rajeshtechgeek@gmail.com>
2017-10-17 15:43:45 +0530

    Event page UI design phase 2

--- app/src/main/java/com/numnu/android/fragments/EventDetailFragment.java
+++ app/src/main/java/com/numnu/android/fragments/EventDetailFragment.java
@@ -12,16 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.numnu.android.R;
 import com.numnu.android.fragments.EventDetail.EventBusinessFragment;
 import com.numnu.android.fragments.EventDetail.EventMenuItemsFragment;
 import com.numnu.android.fragments.EventDetail.EventReviewsFragment;
-import com.numnu.android.fragments.home.EventsFragment;
-import com.numnu.android.fragments.home.MenuItemsFragment;
-import com.numnu.android.fragments.home.PostsFragment;
-import com.numnu.android.fragments.home.UsersFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,16 +46,18 @@ public class EventDetailFragment extends Fragment {
 
         View view=inflater.inflate(R.layout.fragment_event_detail, container, false);
 
-        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        ViewPager viewPager = view.findViewById(R.id.event_viewpager);
         setupViewPager(viewPager);
 
         searchViewFood=view.findViewById(R.id.search_food);
         searchViewLocation=view.findViewById(R.id.search_location);
         TabLayout tabLayout = view.findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+//
+//
         TextView toolbarTitle=view.findViewById(R.id.toolbar_title);
         toolbarTitle.setText(R.string.event);
+
         return view;
     }
 
