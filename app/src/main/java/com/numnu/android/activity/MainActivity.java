0ff59d06b652bcbf5532aa9c9709581298cdb945
Adavan Ayyappan <adavan@czsm.co.in>
2017-10-23 12:05:53 +0530

    Latest changes

--- app/src/main/java/com/numnu/android/activity/MainActivity.java
+++ app/src/main/java/com/numnu/android/activity/MainActivity.java
@@ -1,175 +1,331 @@
-package com.numnu.android.activity;
+package com.motomecha.com.motocube;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.numnu.android.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.motomecha.com.motocube.Adapter.Shopping3Adapter;
+import com.motomecha.com.motocube.Api.FilterApi;
+import com.motomecha.com.motocube.Api.Shopping3Api;
+import com.motomecha.com.motocube.helper.PrefManager;
+import com.motomecha.com.motocube.modelclass.Contact;
+import com.motomecha.com.motocube.modelclass.ContactList;
+import com.squareup.picasso.Picasso;
 
-import java.util.Arrays;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends MyActivity {
-    private static final String TAG ="MainActivity" ;
-    // [START declare_auth]
-    private FirebaseAuth mAuth;
-    // [END declare_auth]
-    private CallbackManager mCallbackManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+public class Shopping4 extends AppCompatActivity implements  AdapterView.OnItemSelectedListener{
+
+    private String TAG = Shopping3.class.getSimpleName();
+    private  boolean isSpinnerTouched = false;
+    Retrofit retrofit = null;
+    GridView gridview4;
+    ProgressDialog pDialog;
+    //    EditText s4;
+    Spinner s4;
+    ArrayList<Contact> contactLists = new ArrayList<Contact>();
+    ArrayList<Contact> spinnerlist = new ArrayList<Contact>();
+    ArrayList<HashMap<String, String>> contactList;
+    Shopping3Adapter adapter;
+    ArrayList<String> countries = new ArrayList<String>();
+    ArrayAdapter adapter1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        FacebookSdk.sdkInitialize(getApplicationContext());
-        setContentView(R.layout.activity_main);
-        TextView textView=findViewById(R.id.textView3);
-        textView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        setContentView(R.layout.shopping4);
+        contactList = new ArrayList<>();
+        gridview4 = (GridView) findViewById(R.id.gridview4);
 
-        mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
 
-        // [START initialize_fblogin]
-        // Initialize Facebook Login button
-        mCallbackManager = CallbackManager.Factory.create();
-        Button loginButton = findViewById(R.id.btn_facebook);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        s4 = (Spinner) findViewById(R.id.search4);
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://motomecha.com/MM_vendor/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Map<String, String> params2 = new HashMap<String, String>();
+        FilterApi api2 = retrofit.create(FilterApi.class);
+        Call<ContactList> call1 = api2.getTopRatedMovies(params2);
+        call1.enqueue(new Callback<ContactList>() {
             @Override
-            public void onClick(View view) {
-                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("email", "public_profile"));
-                LoginManager.getInstance().registerCallback(mCallbackManager,
-                        new FacebookCallback<LoginResult>()
-                        {
-                            @Override
-                            public void onSuccess(LoginResult loginResult)
-                            {
-                                // App code
-                                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                                handleFacebookAccessToken(loginResult.getAccessToken());
-                            }
+            public void onResponse(Call<ContactList> call, Response<ContactList> response) {
+                spinnerlist = response.body().getContacts();
+                Log.e("xsxs", "" + spinnerlist.size());
+                for(int i =0; i < spinnerlist.size(); i++){
 
-                            @Override
-                            public void onCancel()
-                            {
-                                Log.d(TAG, "facebook:onCancel");
-                            }
+                    Log.e("xsxs", "" + spinnerlist.get(i).getFilter());
+                    countries.add(spinnerlist.get(i).getFilter());
 
-                            @Override
-                            public void onError(FacebookException exception)
-                            {
-                                Log.d(TAG, "facebook:onError", exception);
-                            }
-                        });
+                }
+                adapter1 = new ArrayAdapter(Shopping4.this, android.R.layout.simple_spinner_item, countries);
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                s4.setAdapter(adapter1);
 
-                // [END initialize_fblogin]
+            }
+
+            @Override
+            public void onFailure(Call<ContactList> call, Throwable t) {
+
             }
         });
 
-    }
 
 
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://motomecha.com/MM_vendor/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-    // [START on_start_check_user]
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-    }
-    // [END on_start_check_user]
+        Map<String, String> params = new HashMap<String, String>();
+        Shopping3Api api = retrofit.create(Shopping3Api.class);
+        Call<ContactList> call = api.getTopRatedMovies(params);
+        call.enqueue(new Callback<ContactList>() {
+            @Override
+            public void onResponse(Call<ContactList> call, Response<ContactList> response) {
+                contactLists = response.body().getContacts();
+                Log.e("xsxs", "" + contactLists.size());
+                adapter = new Shopping3Adapter(getApplicationContext(), contactLists);
+                gridview4.setAdapter(adapter);
 
-    // [START on_activity_result]
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+//                for(int i =0; i < contactLists.size(); i++){
+//
+//                    Log.e("xsxs", "" + contactLists.get(i).getProductName());
+//                    countries.add(contactLists.get(i).getProductName());
+//
+//                }
+//                adapter1 = new ArrayAdapter(Shopping4.this, android.R.layout.simple_spinner_item, countries);
+//                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                s4.setAdapter(adapter1);
+            }
 
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-    // [END on_activity_result]
+            @Override
+            public void onFailure(Call<ContactList> call, Throwable t) {
 
-    // [START auth_with_facebook]
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
-        // [START_EXCLUDE silent]
-        showProgressDialog();
-        // [END_EXCLUDE]
+            }
+        });
 
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        gridview4.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            TextView prod_nme, prod_prc, prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10, mrp;
+            ImageView prod_img;
+            Button b1, b2;
+            String mobile_number = "";
+            String product_image = "";
+            Spinner quan;
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                final Dialog dialog = new Dialog(Shopping4.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.product_descrip);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                prod_nme = (TextView) dialog.findViewById(R.id.prodnme);
+                prod_prc = (TextView) dialog.findViewById(R.id.prodprc);
+                prod_img = (ImageView) dialog.findViewById(R.id.imageView3);
+                prod1 = (TextView) dialog.findViewById(R.id.proddes);
+                prod2 = (TextView) dialog.findViewById(R.id.proddes1);
+                prod3 = (TextView) dialog.findViewById(R.id.proddes2);
+                prod4 = (TextView) dialog.findViewById(R.id.proddes3);
+                prod5 = (TextView) dialog.findViewById(R.id.proddes4);
+                prod6 = (TextView) dialog.findViewById(R.id.proddes5);
+                prod7 = (TextView) dialog.findViewById(R.id.proddes6);
+                prod8 = (TextView) dialog.findViewById(R.id.proddes7);
+                prod9 = (TextView) dialog.findViewById(R.id.proddes8);
+                prod10 = (TextView) dialog.findViewById(R.id.proddes9);
+                mrp = (TextView) dialog.findViewById(R.id.textView8);
+                b1 = (Button) dialog.findViewById(R.id.bookshop);
+                b2 = (Button) dialog.findViewById(R.id.close);
+                prod_nme.setText(contactLists.get(position).getProductName());
+                prod_prc.setText(contactLists.get(position).getProductPrice());
+                prod1.setText(contactLists.get(position).getProdDescription());
+                prod2.setText(contactLists.get(position).getProdDescription1());
+                prod3.setText(contactLists.get(position).getProdDescription2());
+                prod4.setText(contactLists.get(position).getProdDescription3());
+                prod5.setText(contactLists.get(position).getProdDescription4());
+                prod6.setText(contactLists.get(position).getProdDescription5());
+                prod7.setText(contactLists.get(position).getProdDescription6());
+                prod8.setText(contactLists.get(position).getProdDescription7());
+                prod9.setText(contactLists.get(position).getProdDescription8());
+                prod10.setText(contactLists.get(position).getProdDescription9());
+                mrp.setText(contactLists.get(position).getMrpPrice());
+                Picasso.with(Shopping4.this).load(contactLists.get(position).getProductImage()).into(prod_img);
+                PrefManager pref = new PrefManager(Shopping4.this);
+                mobile_number = pref.getMobileNumber();
+                product_image = contactLists.get(position).getProductImage();
+                quan = (Spinner) dialog.findViewById(R.id.spinner3);
+                final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(Shopping4.this,
+                        R.array.SELECT, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+                quan.setAdapter(adapter);
+                dialog.show();
+                b2.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
+                    public void onClick(View v) {
+                        // Close dialog
+                        dialog.dismiss();
                     }
                 });
+
+                b1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        final String product_name = prod_nme.getText().toString();
+                        final String product_price = prod_prc.getText().toString();
+                        final String quantity = quan.getSelectedItem().toString();
+
+                        com.android.volley.Response.Listener<String> responseListener = new com.android.volley.Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    boolean success = jsonResponse.getBoolean("success");
+                                    if (success) {
+                                        Toast.makeText(Shopping4.this,
+                                                " Your Item has been added to cart ", Toast.LENGTH_LONG).show();
+                                        dialog.dismiss();
+                                    } else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(Shopping4.this);
+                                        builder.setMessage("Register Failed")
+                                                .setNegativeButton("Retry", null)
+                                                .create()
+                                                .show();
+
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        ProdDescRequest prodDescRequest = new ProdDescRequest(product_name, product_price, mobile_number, quantity, product_image, responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(Shopping4.this);
+                        queue.add(prodDescRequest);
+
+                    }
+                });
+
+
+            }
+
+
+        });
+        s4.setOnItemSelectedListener(Shopping4.this);
+        s4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                isSpinnerTouched = true;
+                return false;
+            }
+        });
     }
-    // [END auth_with_facebook]
+//    private void setupSearchView()
+//    {
+//        s4.setIconifiedByDefault(false);
+//        s4.setOnQueryTextListener(this);
+//        s4.setSubmitButtonEnabled(true);
+//        s4.setQueryHint("Search Here");
+//    }
+//
+//
+//    public boolean onQueryTextChange(String newText)
+//    {
+//
+//        if (TextUtils.isEmpty(newText)) {
+//            gridview4.clearTextFilter();
+//        } else {
+//            gridview4.setFilterText(newText);
+//        }
+//        return true;
+//    }
+//
+//
+//    public boolean onQueryTextSubmit(String query)
+//    {
+//        return false;
+//    }
 
-    public void signOut() {
-        mAuth.signOut();
-        LoginManager.getInstance().logOut();
 
+    @Override
+    public void onItemSelected(AdapterView a, View v, final int i, long lng) {
+
+        if (!isSpinnerTouched) return;
+        pDialog=new ProgressDialog(this);
+        pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        pDialog.setMessage("Please wait..");
+        pDialog.setCancelable(true);
+        pDialog.setIndeterminate(true);
+        pDialog.show();
+        long delayInMillis = 3000;
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                pDialog.dismiss();
+            }
+        }, delayInMillis);
+
+        String text = s4.getSelectedItem().toString();
+        ArrayList<Contact> filtercontactLists = new ArrayList<>();
+
+        if (contactLists.size() > 1) {
+
+            for(int j=0;j < contactLists.size(); j++) {
+
+                Log.e("jjj", "" + contactLists.get(j).getProdDescription0());
+                Log.e("xsjkhkhkhkhxs", "" + text);
+
+                if ("Pulsar" == text) {
+
+                    filtercontactLists.add(contactLists.get(j));
+                    Log.e("xsjkhkhkhkhxs", "" + contactLists.get(j));
+
+                }
+            }
+
+            Log.e("xsjkhkhkhkhxs", "" + filtercontactLists.size());
+            adapter = new Shopping3Adapter(getApplicationContext(), filtercontactLists);
+            gridview4.setAdapter(adapter);
+
+        }
     }
+    @Override
+    public void onNothingSelected(AdapterView<?> parentView)
+    {
 
 
-    public void createAccount(View view) {
-        Intent mainIntent = new Intent(MainActivity.this,SignupActivity.class);
-        MainActivity.this.startActivity(mainIntent);
-
-    }
-
-    public void signIn(View view) {
-        Intent mainIntent = new Intent(this,LoginActivity.class);
-        this.startActivity(mainIntent);
     }
 
 }
-
-
-
-
-
