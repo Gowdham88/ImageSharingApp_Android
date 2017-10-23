a40ecb7038beb5324742f2ae1882bc3862565d08
Rajesh K <rajeshtechgeek@gmail.com>
2017-10-23 18:52:38 +0530

    Event page UI design phase 3

--- app/src/main/java/com/numnu/android/fragments/EventDetailFragment.java
+++ app/src/main/java/com/numnu/android/fragments/EventDetailFragment.java
@@ -1,15 +1,20 @@
 package com.numnu.android.fragments;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -29,6 +34,9 @@ public class EventDetailFragment extends Fragment {
 
     SearchView searchViewFood,searchViewLocation;
     private Context context;
+    TextView weblink1,weblink2,weblink3;
+    private TextView viewEventMap,eventDescription,eventName,city,eventDate,eventTime;
+    private ImageView eventImageView;
 
     public static EventDetailFragment newInstance() {
         return new EventDetailFragment();
@@ -49,6 +57,29 @@ public class EventDetailFragment extends Fragment {
         ViewPager viewPager = view.findViewById(R.id.event_viewpager);
         setupViewPager(viewPager);
 
+        weblink1 = view.findViewById(R.id.txt_weblink_1);
+        weblink2 = view.findViewById(R.id.txt_weblink_2);
+        weblink3 = view.findViewById(R.id.txt_weblink_3);
+
+        viewEventMap = view.findViewById(R.id.txt_view_event_map);
+        eventDescription = view.findViewById(R.id.event_description);
+        eventName = view.findViewById(R.id.event_name);
+        city = view.findViewById(R.id.txt_city);
+        eventDate = view.findViewById(R.id.txt_event_date);
+        eventTime = view.findViewById(R.id.txt_event_time);
+
+
+
+
+
+        eventImageView = view.findViewById(R.id.current_event_image);
+
+
+//        Picasso.with(activity).load(url).transform(new RoundedCornersTransform(this)).into(eventImageView)
+
+
+        setupWebLinks();
+
         searchViewFood=view.findViewById(R.id.search_food);
         searchViewLocation=view.findViewById(R.id.search_location);
         TabLayout tabLayout = view.findViewById(R.id.tabs);
@@ -61,6 +92,25 @@ public class EventDetailFragment extends Fragment {
         return view;
     }
 
+    private void setupWebLinks() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            weblink1.setTextColor(getResources().getColor(R.color.blue,null));
+            weblink2.setTextColor(getResources().getColor(R.color.blue,null));
+            weblink3.setTextColor(getResources().getColor(R.color.blue,null));
+            viewEventMap.setTextColor(getResources().getColor(R.color.blue,null));
+        }else {
+            weblink1.setTextColor(getResources().getColor(R.color.blue));
+            weblink2.setTextColor(getResources().getColor(R.color.blue));
+            weblink3.setTextColor(getResources().getColor(R.color.blue));
+            viewEventMap.setTextColor(getResources().getColor(R.color.blue));
+        }
+        weblink1.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        weblink2.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        weblink3.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        viewEventMap.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+    }
 
 
     private void setupViewPager(ViewPager viewPager) {
