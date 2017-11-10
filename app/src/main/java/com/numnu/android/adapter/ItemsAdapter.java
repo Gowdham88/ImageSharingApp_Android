f4e15291df2a2d4ed9edde46221065a010e87456
poojitha <poojithanalluri80@gmail.com>
2017-11-10 15:25:23 +0530

    Business screen modifications

--- app/src/main/java/com/numnu/android/adapter/ItemsAdapter.java
+++ app/src/main/java/com/numnu/android/adapter/ItemsAdapter.java
@@ -5,47 +5,53 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.numnu.android.R;
 
+
 /**
- * Created by lenovo on 11/1/2017.
+ * Created by lenovo on 10/5/2017.
  */
 
-public class PostsTabadapter extends RecyclerView.Adapter<PostsTabadapter.ViewHolder> {
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
 
-    String[] arr = {"Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category", "Category",};
+    String[] arr = {"Festival", "Wine", "Party", "Rum", "Festival", "Wine", "Party", "Rum", "Festival", "Wine", "Party", "Rum",};
     Context context;
     LayoutInflater layout;
 
-    public PostsTabadapter(Context context) {
+    public ItemsAdapter(Context context) {
         this.context = context;
         layout = LayoutInflater.from(context);
 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView PostcategoryTxt,postcountTxt;
+        TextView ItemTxt,CountTxt;
+        ImageView Itemimg;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
-            PostcategoryTxt=(TextView)itemView.findViewById(R.id.post_category_text);
-            postcountTxt=(TextView)itemView.findViewById(R.id.post_count_text);
+            ItemTxt=(TextView)itemView.findViewById(R.id.item_category_text);
+            Itemimg=(ImageView)itemView.findViewById(R.id.item_category_img);
+            CountTxt=(TextView)itemView.findViewById(R.id.item_count_text);
+
         }
     }
 
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewtype) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_content, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.menuitems_content, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder Holder, final int position) {
 
-        Holder.PostcategoryTxt.setText(arr[position]);
+        Holder.ItemTxt.setText(arr[position]);
 
 //
 
@@ -56,5 +62,4 @@ public class PostsTabadapter extends RecyclerView.Adapter<PostsTabadapter.ViewHo
         return arr.length;
     }
 }
-
 
